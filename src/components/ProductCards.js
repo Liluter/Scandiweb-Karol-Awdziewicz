6d042d61e53dd435ca5bae68319ef9dad9b196c6
liluter <karolawdziewicz@gmail.com>
2022-09-09 23:00:46 +0200

    Add scroll bottom hit trigger

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import ProductCard from './ProductCard'
 import {gql} from '@apollo/client'
 import {Query} from '@apollo/client/react/components'
+import {graphql } from '@apollo/client/react/hoc'
 import '../styles/ProductCards.scss'
+import {ReactComponent as Spinner} from '../assets/Spinner.svg'
 
 const GET_PRODUCTS = gql`
 query GetProducts($input: CategoryInput) {
@@ -30,27 +32,110 @@ query GetProducts($input: CategoryInput) {
 export class ProductCards extends Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {pageNumber: 1,
+                  pageOneLimit: 4, // change first number of items on the page to show scroll
+                  itemPerPage: 2,  // numbers item fetch form db in next scroll down
+                  spinner: false}
+    // this.scroll = this.scroll.bind(this)
+    this.handlePageNumber = this.handlePageNumber.bind(this)
+    // this.handlePagination = this.handlePagination.bind(this)
   }
-
-
-  // componentDidMount(){
-  //   console.log('Products Cards mount',this.props);
+  componentDidMount(){
+    const windowXsize = window.screen.availWidth
+    if (windowXsize < 983) {
+      this.setState(()=> ({ pageOneLimit: 2, itemPerPage: 1}))
+    } else if (windowXsize < 1381) {
+      this.setState(()=> ({ pageOneLimit: 3, itemPerPage: 2}))
+    } else if (windowXsize < 1440) {
+      this.setState(()=> ({ pageOneLimit: 4, itemPerPage: 3}))
+    }  else {
+      this.setState(()=> ({ pageOneLimit: 4, itemPerPage: 3}))
+    }
+    this.handlePageNumber()
+  }
+  
+  handlePageNumber(){
+    window.addEventListener('scroll', ()=> {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const PagesLimit = Math.ceil( (this.props.data.category.products.length - this.state.pageOneLimit) / this.state.itemPerPage) 
+    // console.log('PAGE LIMIT', PagesLimit)
+    // console.log('Aktual number in page:', ( (this.state.itemPerPage  * (this.state.pageNumber - 1)) + this.state.pageOneLimit  ))
+      if ( (scrollTop + clientHeight) === scrollHeight ) {        
+            if (this.state.pageNumber <= (PagesLimit) ){
+              this.props.handleSpinner()
+                .then((res) =>  {
+                  this.setState(()=>({...this.state , pageNumber: this.state.pageNumber + 1})) 
+                  })
+            }
+      } 
+    } , {passive: true})
+  }
+  
+  // handlePagination(){
+  //   const {pageNumber, pageOneLimit, itemPerPage }= this.state
+  //   return (pageOneLimit + pageNumber*itemPerPage)
   // }
 
+
   render() {
+    // console.log('Product Cards Props',this.props)
+    console.log('Product Cards State',this.state, window.screen.availWidth)
+    // console.log('HANDLE PAGINATION RESOLVE', this.handlePagination())
     return (
       <div className='productCards'>
       <Query query={GET_PRODUCTS} variables={{"input": {"title": this.props.category}}} >
           {({loading, data})=>{
             if (loading) return "Loading...";
             const { category} = data;
-            return (<>{category.products.map((product,index)=> <ProductCard key={product.id} product={product}  />)}</>)
+            // const PageLimit = Math.ceil(data.category.products.length / this.state.itemLimit)
+            // console.log('PageLimit', PageLimit)
+
+
+
+            return (<> {category.products.map((product,index)=> ( index < ((this.state.itemPerPage  * (this.state.pageNumber - 1)) + this.state.pageOneLimit  ) ? <ProductCard key={product.id} product={product}/> : null )  )}  </>)
+
+
+            // return (<>{category.products.map((product,index)=> <ProductCard key={product.id} product={product}  />)}</>)
           }}
       </Query>
+      
       </div>
     )
   }
 }
 
-export default ProductCards;
+export default graphql(GET_PRODUCTS, {
+  options: (props) => {
+    return {
+      variables: {
+        input: {title: props.category}
+    }
+  }
+}})(ProductCards);
+
+
+// componentDidMount(){
+//   // console.log('Scroll: ',this.scroll())
+//   window.addEventListener('scroll', ()=> {
+//     const {
+//         scrollTop,
+//         scrollHeight,
+//         clientHeight
+//     } = document.documentElement;
+//     if (scrollTop + clientHeight >= scrollHeight - 5 ) {
+//       console.log('Scroll on bottom !!!', {scroll: scrollTop, scrollHeight: scrollHeight, clienHeight: clientHeight} )
+//     }
+//   } , {passive: true})
+// }
+// componentDidUpdate(){
+//   // console.log('Scroll: ',this.scroll())
+  
+// }
+// scroll(){
+//   const {
+//       scrollTop,
+//       scrollHeight,
+//       clientHeight
+//   } = document.documentElement;
+//   return {scroll: scrollTop, scrollHeight: scrollHeight, clienHeight: clientHeight}
+// }
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import ProductCards from '../components/ProductCards'
 import '../styles/Category.scss'
-
+import {ReactComponent as Spinner} from '../assets/Spinner.svg'
 
 class Category extends React.Component {
   constructor(props){
     super(props);
-    this.state={}
+    this.state = {  spinner: false,
+                    data: true  }
+    this.handleSpinner = this.handleSpinner.bind(this)
   }
-  // componentDidMount(){
-  //   console.log('Category mount',this.props)
-  // }
-  // componentDidUpdate(){
-  //   console.log('Category update',this.props)
-  // }
+
+handleSpinner(){
+  return new Promise ( (resolve, reject)=> {
+    const { scrollHeight, clientHeight } = document.documentElement;
+    this.setState(()=>({spinner: true}))
+    document.documentElement.scrollTop = scrollHeight - clientHeight - 6;
+    setTimeout(()=> { 
+      this.setState(()=>({spinner: false})); 
+      resolve( true );
+    },1500 )
+  })
+}
 
   render() {
+    
     return (
     <main className='category background'>
       <header className='category__title'><span>{this.props.category[0].toUpperCase() + this.props.category.slice(1)}</span></header>
       <ProductCards key={this.props.category}
-      category={this.props.category} 
+      category={this.props.category}
+      handleSpinner={this.handleSpinner}
+      // spinner={this.state.spinner}
       // currentCurency={this.props.currentCurency}
       />
+      <div className='spinnerWrapper'>
+        <Spinner className={`spinnerWrapper--spinner ${ this.state.spinner ? '' : 'hide'  }`} /> 
+      </div> 
     </main>
     )
   }
